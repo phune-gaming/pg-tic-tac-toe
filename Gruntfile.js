6f7ced2193b4c38182bc672d9aa5d7c41abba5bd
Samuel Santos <ssantos@present-technologies.com>
2014-04-09 16:19:51 +0100

    Remove unnecessary quotes from urls in CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
             }
         },
         clean: {
-            all: ['bin', 'dist', '.tmp']
+            all: ['bin', 'dist/*', '.tmp']
         },
         jshint: {
             options: {
